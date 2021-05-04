2e23ff4778369e8a74af931add95b035f47a66c4
berlinsky <berlinsky.yar@gmail.com>
2021-05-05 00:33:46 +0300

    LogInVC fixed

--- OnlineStore/ViewControllers/LogInViewController.swift
+++ OnlineStore/ViewControllers/LogInViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
-    @IBOutlet weak var firstNameTextField: UITextField!
-    
-    @IBOutlet weak var lastNameTextField: UITextField!
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var logInButton: UIButton!
     
@@ -19,29 +19,76 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setUpElements()
-        // Do any additional setup after loading the view.
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
     }
+    
     
     func setUpElements () {
         errorLabel.alpha = 0
         
-        Utilities.styleTextField(firstNameTextField)
-        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(logInButton)
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInTapped() {
+        guard emailTextField.text != "" && passwordTextField.text != ""
+        else {
+            errorMessage(title: "Ooops!", message: "Please, fill all the fields")
+            return
+        }
+        if isDataMatches(email: emailTextField.text!, password: passwordTextField.text!) {
+            errorLabel.alpha = 0
+            print("Done!")
+            performSegue(withIdentifier: "loginedSuccessfully", sender: nil)
+            dismiss(animated: true, completion: nil)
+        }
+        else{
+            errorLabel.alpha = 1
+            errorLabel.text = "Incorrect email or password"
+        }
     }
-    */
-    @IBAction func logInTapped(_ sender: Any) {
+
+    }
+
+extension LogInViewController: UITextFieldDelegate {
+    private func errorMessage(title: String, message: String, textField: UITextField? = nil){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default){
+            _ in textField?.text = nil
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func isDataMatches(email: String, password: String) -> Bool{
+        print(DATABASE.count)
+        for i in 0..<DATABASE.count {
+            if DATABASE[i].email == email {
+                if DATABASE[i].password == password {
+                        return true
+                    }
+                }
+            }
+        return false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInTapped()
+        }
+        return true
     }
     
 }
